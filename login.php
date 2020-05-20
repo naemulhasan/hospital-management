a5cf325f7b9deb0d3fd24b9635e4559376726e28
naemulhasan <60178100+naemulhasan@users.noreply.github.com>
2020-05-20 03:21:46 -0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Login Page</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+	<form class="box" method="POST" action="loginverify.php">
+	
+
+		
+							<h1><b>Login</b></h1>
+							 <label><b>Username</b></label>
+							<input type="text" name="uid" value="" placeholder="Username"> <br>
+							 <label><b>Password</b></label>
+							<input type="password" name="upassword" placeholder="password"> <br>
+							<input type="submit" name="submit" value="Log In">
+							<a href="register.php">Registration</a>
+				
+		
+	</form>
+</body>
+</html>
